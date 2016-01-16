ce38c09e19449b55a785285bcc4c2d001a91ca79
Harri Feng <harrifeng@gmail.com>
2016-01-16 22:19:47 +0800

    new seven example

---
+++ src/main/java/org/hfeng/seven/threadslocks/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.hfeng.seven.threadslocks;
+
+public class HelloWorld {
+    public static void main(String[] args)  throws InterruptedException {
+        Thread myThread = new Thread() {
+                public void run() {
+                    System.out.println("Hello from new thread");
+                }
+            };
+        myThread.start();
+        Thread.yield();
+        System.out.println("Hello from main thread");
+        myThread.join();
+    }
+}
